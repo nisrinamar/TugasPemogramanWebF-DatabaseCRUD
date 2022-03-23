6f5074193302c123ac36ff9745bbcc504d99322e
nisrinamar <99307277+nisrinamar@users.noreply.github.com>
2022-03-23 14:12:39 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'Model/Peserta.php';
+
+if(adaPesan()){
+    $pesan = $_GET['pesan'];
+} else $pesan = "";
+
+$daftarPeserta = Peserta::getAll();
+
+tampilkanHalamanIndex($pesan,$daftarPeserta);
+
+function adaPesan(){
+    if(isset($_GET['pesan'])){
+        return true;
+    } else return false;
+}
+
+function tampilkanHalamanIndex($pesan,$daftarPeserta){
+    require_once 'view-indeks.php';
+}
+?>
